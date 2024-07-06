5adbc8939ca10b4ee09fb0c55fb096178ab0afb1
chan000518 <128445671+chan000518@users.noreply.github.com>
2024-07-06 11:34:38 +0900

    [add] 에러 핸들러 추가, product 유효성 검사

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
 import { assert } from 'superstruct';
 import { CreateUser, PatchUser } from './structs.js';
+import { CreateProduct, PatchProduct } from './structs.js';
+
 
 const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 
-app.get('/users', async (req, res) => {
+// 에러 처리
+function asyncHandler(handler) {
+  return async function (req, res) {
+    try {
+      await handler(req, res);
+    } catch (e) {
+      if (e.name === 'StructError' ||
+        e instanceof Prisma.PrismaClientValidationError
+      ) {
+        res.status(400).send({ message: e.message });
+      } else if (
+        e instanceof Prisma.PrismaClientKnownRequestError
+        && e.code === 'P2025'
+      ) {
+        res.sendStatus(404);
+      } else {
+        res.status(500).send({ message: e.message });
+      }
+    }
+  };
+}
+
+app.get('/users', asyncHandler(async (req, res) => {
   // 구조 분해 할당
   const { offset = 0, limit = 10, order = "newest"} = req.query
   let orderBy;
@@ -29,17 +53,17 @@ app.get('/users', async (req, res) => {
   });
 
   res.send(users);
-});
+}));
 
-app.get('/users/:id', async (req, res) => {
+app.get('/users/:id', asyncHandler(async (req, res) => {
   const { id } = req.params;
   const user = await prisma.user.findUnique({
     where: { id },
   });
   res.send(user);
-});
+}));
 
-app.post('/users', async (req, res) => {
+app.post('/users', asyncHandler(async (req, res) => {
   // 유효성 검사
   assert(req.body, CreateUser);
   // 리퀘스트 바디 내용으로 유저 생성
@@ -47,9 +71,9 @@ app.post('/users', async (req, res) => {
     data: req.body,
   });
   res.status(201).send(user);
-});
+}));
 
-app.patch('/users/:id', async (req, res) => {
+app.patch('/users/:id', asyncHandler(async (req, res) => {
   // 유효성 검사
   assert(req.body, PatchUser);
   const { id } = req.params;
@@ -59,20 +83,20 @@ app.patch('/users/:id', async (req, res) => {
     data: req.body,
   })
   res.send(user);
-});
+}));
 
-app.delete('/users/:id', async (req, res) => {
+app.delete('/users/:id', asyncHandler(async (req, res) => {
   const { id } = req.params;
   // id에 해당하는 유저 삭제
   await prisma.user.delete({
     where: { id },
   })
   res.sendStatus(204);
-});
+}));
 
 /*********** products ***********/
 
-app.get('/products', async (req, res) => {
+app.get('/products', asyncHandler(async (req, res) => {
   // 상품 목록 조회
   const { order = "newest", offset = 0, limit = 10, category = null } = req.query;
   let orderBy;
@@ -99,26 +123,28 @@ app.get('/products', async (req, res) => {
   });
   
   res.send(products);
-});
+}));
 
-app.get('/products/:id', async (req, res) => {
+app.get('/products/:id', asyncHandler(async (req, res) => {
   const { id } = req.params;
   // id에 해당하는 상품 조회
   const product = await prisma.product.findUnique({
     where: { id },
   });
   res.send(product);
-});
+}));
 
-app.post('/products', async (req, res) => {
+app.post('/products', asyncHandler(async (req, res) => {
+  assert(req.body, CreateProduct);
   // 리퀘스트 바디 내용으로 상품 생성
   const product = await prisma.product.create({
     data: req.body,
   })
   res.status(201).send(product);
-});
+}));
 
-app.patch('/products/:id', async (req, res) => {
+app.patch('/products/:id', asyncHandler(async (req, res) => {
+  assert(req.body, PatchProduct);
   const { id } = req.params;
   // 리퀘스트 바디 내용으로 id에 해당하는 상품 수정
   const product = await prisma.product.update({
@@ -126,16 +152,15 @@ app.patch('/products/:id', async (req, res) => {
     data: req.body,
   })
   res.send(product);
-});
+}));
 
-app.delete('/products/:id', async (req, res) => {
+app.delete('/products/:id', asyncHandler(async (req, res) => {
   const { id } = req.params;
   // id에 해당하는 상품 삭제
   await prisma.product.update({
     where: {id},
   })
   res.sendStatus(204);
-});
-
+}));
 
 app.listen(process.env.PORT || 3000, () => console.log('Server Started'));
--- structs.js
+++ structs.js
@@ -7,9 +7,19 @@ export const CreateUser = s.object({
     email : s.define('Email', isEmail),
     // 사이즈 제한
     firstName: s.size(s.string(), 1, 30),
-    lastName: s.string(s.string(), 1 ,30),
+    lastName: s.size(s.string(), 1, 30),
     // 타입  검사
     address: s.string(),
 });
 
 export const PatchUser = s.partial(CreateUser);
+
+export const CreateProduct = s.object({
+    name: s.size(s.string(), 1, 60),
+    description: s.string(),
+    category: s.enums(['FASHION', 'SPORTS', 'ELECTRONICS', 'HOME_INTERIOR', 'HOUSEHOLD_SUPPLIES', 'KITCHENWARE']),
+    price: s.min(s.number(), 0),
+    stock: s.min(s.integer(), 0),
+});
+
+export const PatchProduct = s.partial(CreateProduct);

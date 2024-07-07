248a6072d3b03cf825c4cfc74747b9209b87dfd8
chan000518 <128445671+chan000518@users.noreply.github.com>
2024-07-07 14:38:09 +0900

    [add] 데이터와 처리 라우트 추가

--- app.js
+++ app.js
@@ -3,28 +3,33 @@ dotenv.config();
 import express from 'express';
 import { PrismaClient, Prisma } from '@prisma/client';
 import { assert } from 'superstruct';
-import { CreateUser, PatchUser } from './structs.js';
-import { CreateProduct, PatchProduct } from './structs.js';
-
+import {
+  CreateUser,
+  PatchUser,
+  CreateProduct,
+  PatchProduct,
+  CreateOrder,
+  PatchOrder,
+} from './structs.js';
 
 const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 
-// 에러 처리
 function asyncHandler(handler) {
   return async function (req, res) {
     try {
       await handler(req, res);
     } catch (e) {
-      if (e.name === 'StructError' ||
+      if (
+        e.name === 'StructError' ||
         e instanceof Prisma.PrismaClientValidationError
       ) {
         res.status(400).send({ message: e.message });
       } else if (
-        e instanceof Prisma.PrismaClientKnownRequestError
-        && e.code === 'P2025'
+        e instanceof Prisma.PrismaClientKnownRequestError &&
+        e.code === 'P2025'
       ) {
         res.sendStatus(404);
       } else {
@@ -34,39 +39,37 @@ function asyncHandler(handler) {
   };
 }
 
+/*********** users ***********/
+
 app.get('/users', asyncHandler(async (req, res) => {
-  // 구조 분해 할당
-  const { offset = 0, limit = 10, order = "newest"} = req.query
+  const { offset = 0, limit = 10, order = 'newest' } = req.query;
   let orderBy;
-  switch(order) {
+  switch (order) {
     case 'oldest':
-      orderBy = {createAt : 'asc'};
+      orderBy = { createdAt: 'asc' };
       break;
     case 'newest':
     default:
-      orderBy = {createAt: 'desc'};
+      orderBy = { createdAt: 'desc' };
   }
   const users = await prisma.user.findMany({
     orderBy,
     skip: parseInt(offset),
     take: parseInt(limit),
   });
-
   res.send(users);
 }));
 
 app.get('/users/:id', asyncHandler(async (req, res) => {
   const { id } = req.params;
-  const user = await prisma.user.findUnique({
+  const user = await prisma.user.findUniqueOrThrow({
     where: { id },
   });
   res.send(user);
 }));
 
 app.post('/users', asyncHandler(async (req, res) => {
-  // 유효성 검사
   assert(req.body, CreateUser);
-  // 리퀘스트 바디 내용으로 유저 생성
   const user = await prisma.user.create({
     data: req.body,
   });
@@ -74,31 +77,27 @@ app.post('/users', asyncHandler(async (req, res) => {
 }));
 
 app.patch('/users/:id', asyncHandler(async (req, res) => {
-  // 유효성 검사
   assert(req.body, PatchUser);
   const { id } = req.params;
-  // 리퀘스트 바디 내용으로 id에 해당하는 유저 수정
   const user = await prisma.user.update({
     where: { id },
     data: req.body,
-  })
+  });
   res.send(user);
 }));
 
 app.delete('/users/:id', asyncHandler(async (req, res) => {
   const { id } = req.params;
-  // id에 해당하는 유저 삭제
   await prisma.user.delete({
     where: { id },
-  })
+  });
   res.sendStatus(204);
 }));
 
 /*********** products ***********/
 
 app.get('/products', asyncHandler(async (req, res) => {
-  // 상품 목록 조회
-  const { order = "newest", offset = 0, limit = 10, category = null } = req.query;
+  const { offset = 0, limit = 10, order = 'newest', category } = req.query;
   let orderBy;
   switch (order) {
     case 'priceLowest':
@@ -114,20 +113,18 @@ app.get('/products', asyncHandler(async (req, res) => {
     default:
       orderBy = { createdAt: 'desc' };
   }
-
+  const where = category ? { category } : {};
   const products = await prisma.product.findMany({
+    where,
     orderBy,
     skip: parseInt(offset),
     take: parseInt(limit),
-    where: category ? { category } : {},
   });
-  
   res.send(products);
 }));
 
 app.get('/products/:id', asyncHandler(async (req, res) => {
   const { id } = req.params;
-  // id에 해당하는 상품 조회
   const product = await prisma.product.findUnique({
     where: { id },
   });
@@ -136,30 +133,66 @@ app.get('/products/:id', asyncHandler(async (req, res) => {
 
 app.post('/products', asyncHandler(async (req, res) => {
   assert(req.body, CreateProduct);
-  // 리퀘스트 바디 내용으로 상품 생성
   const product = await prisma.product.create({
     data: req.body,
-  })
+  });
   res.status(201).send(product);
 }));
 
 app.patch('/products/:id', asyncHandler(async (req, res) => {
   assert(req.body, PatchProduct);
   const { id } = req.params;
-  // 리퀘스트 바디 내용으로 id에 해당하는 상품 수정
   const product = await prisma.product.update({
-    where: {id},
+    where: { id },
     data: req.body,
-  })
+  });
   res.send(product);
 }));
 
 app.delete('/products/:id', asyncHandler(async (req, res) => {
   const { id } = req.params;
-  // id에 해당하는 상품 삭제
-  await prisma.product.update({
-    where: {id},
-  })
+  await prisma.product.delete({
+    where: { id },
+  });
+  res.sendStatus(204);
+}));
+
+/*********** orders ***********/
+
+app.get('/orders', asyncHandler(async (req, res) => {
+  const orders = await prisma.order.findMany();
+  res.send(orders);
+}));
+
+app.get('/orders/:id', asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const order = await prisma.order.findUniqueOrThrow({
+    where: { id },
+  });
+  res.send(order);
+}));
+
+app.post('/orders', asyncHandler(async (req, res) => {
+  assert(req.body, CreateOrder);
+  const order = await prisma.order.create({
+    data: req.body,
+  });
+  res.status(201).send(order);
+}));
+
+app.patch('/orders/:id', asyncHandler(async (req, res) => {
+  assert(req.body, PatchOrder);
+  const { id } = req.params;
+  const order = await prisma.order.update({
+    where: { id },
+    data: req.body,
+  });
+  res.send(order);
+}));
+
+app.delete('/orders/:id', asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  await prisma.order.delete({ where: { id } });
   res.sendStatus(204);
 }));
 
--- structs.js
+++ structs.js
@@ -1,25 +1,44 @@
 import * as s from 'superstruct';
 import isEmail from 'is-email';
+import isUuid from 'is-uuid';
+
+const CATEGORIES = [
+  'FASHION',
+  'BEAUTY',
+  'SPORTS',
+  'ELECTRONICS',
+  'HOME_INTERIOR',
+  'HOUSEHOLD_SUPPLIES',
+  'KITCHENWARE',
+];
+
+const STATUSES = ['PENDING', 'COMPLETE'];
+
+const Uuid = s.define('Uuid', (value) => isUuid.v4(value));
 
 export const CreateUser = s.object({
-    // 새로운 타입 생성
-    // isEmail : 문자열을 받아 불리언을 반환하는 함수
-    email : s.define('Email', isEmail),
-    // 사이즈 제한
-    firstName: s.size(s.string(), 1, 30),
-    lastName: s.size(s.string(), 1, 30),
-    // 타입  검사
-    address: s.string(),
+  email: s.define('Email', isEmail),
+  firstName: s.size(s.string(), 1, 30),
+  lastName: s.size(s.string(), 1, 30),
+  address: s.string(),
 });
 
 export const PatchUser = s.partial(CreateUser);
 
 export const CreateProduct = s.object({
-    name: s.size(s.string(), 1, 60),
-    description: s.string(),
-    category: s.enums(['FASHION', 'SPORTS', 'ELECTRONICS', 'HOME_INTERIOR', 'HOUSEHOLD_SUPPLIES', 'KITCHENWARE']),
-    price: s.min(s.number(), 0),
-    stock: s.min(s.integer(), 0),
+  name: s.size(s.string(), 1, 60),
+  description: s.string(),
+  category: s.enums(CATEGORIES),
+  price: s.min(s.number(), 0),
+  stock: s.min(s.integer(), 0),
 });
 
 export const PatchProduct = s.partial(CreateProduct);
+
+export const CreateOrder = s.object({
+  userId: Uuid,
+});
+
+export const PatchOrder = s.object({
+  status: s.enums(STATUSES),
+});
